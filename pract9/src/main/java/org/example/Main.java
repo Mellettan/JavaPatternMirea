06aea78dfb9b45e45bd719555f8162127061944d
Mellettan <roma23470@gmail.com>
2024-04-18 00:19:34 +0300

    pract_9

--- pract9/src/main/java/org/example/Main.java
+++ pract9/src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hell0, world");
+        System.out.println("Hello, world");
     }
 }

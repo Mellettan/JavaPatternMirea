7af9104f9f71d015b04ae62c2363572aa6a79202
Mellettan <roma23470@gmail.com>
2024-05-23 14:35:59 +0300

    pract_19

---
+++ pract_19/src/test/java/ru/mirea/pract_19/Pract19ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.pract_19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pract19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
